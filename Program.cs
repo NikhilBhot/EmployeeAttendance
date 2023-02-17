ebb5daf7f81af24ab850a291244c155036a72b39
unknown <bhotnikhil25@gmail.com>
2023-02-17 08:52:07 +0530

    [Nikhil]-Solve using Switch statement

--- Program.cs
+++ Program.cs
@@ -2,32 +2,32 @@
 {
     internal class Program
     {
+        //UC-04 Solve Using Switch statement
+        public const  int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            //UC-02-Calculate Daily Employee Wage
-            int IS_FULL_TIME = 2;
-
-            //UC-03-Add Part Time Employee And Wage
-            int IS_PART_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
 
             Random random= new Random();
-            int empCheck=random.Next(0,2);
+            int empCheck=random.Next(0,3);
 
-            if(empCheck==IS_PART_TIME) 
+            switch (empCheck)
             {
-                empHrs = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME: 
+                    empWage = 8;
+                    break;
+                default: 
+                    empHrs = 0;
+                    break;
             }
-            else if(empCheck==IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs= 0;
-            }
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Daily Employee Wage:" + empWage);
         }
