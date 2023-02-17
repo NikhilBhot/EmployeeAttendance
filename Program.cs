8b15f8b1ff0bf96c6b902cf2ee245d2c93096cf2
unknown <bhotnikhil25@gmail.com>
2023-02-17 08:43:30 +0530

    [Nikhil]-Add Part time Employee Wage

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@
         static void Main(string[] args)
         {
             //UC-02-Calculate Daily Employee Wage
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 2;
+
+            //UC-03-Add Part Time Employee And Wage
+            int IS_PART_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
@@ -13,13 +16,17 @@
             Random random= new Random();
             int empCheck=random.Next(0,2);
 
-            if(empCheck==IS_FULL_TIME ) 
+            if(empCheck==IS_PART_TIME) 
+            {
+                empHrs = 4;
+            }
+            else if(empCheck==IS_FULL_TIME)
             {
                 empHrs = 8;
             }
             else
             {
-                empHrs = 0;
+                empHrs= 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Daily Employee Wage:" + empWage);
